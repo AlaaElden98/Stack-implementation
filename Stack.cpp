9592c9794cd86a03f6daef1ca9e69fec9f7a650c
AlaaElden98 <34111697+AlaaElden98@users.noreply.github.com>
2018-12-20 21:14:51 +0200

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,11 +1,43 @@
-#include "Stack.h"
+#include "Satck.h"
 
-Stack::Stack()
-{
-    //ctor
+stack::stack(){
+    cp=1;
+    sz=0;
+    arr =new int[cp];
+
+}
+void stack::duplicate(){
+cp*=2;
+    int* tmp = new int[cp];
+    for(int i = 0 ; i < sz ; ++i)
+        tmp[i] = arr[i];
+    delete [] arr;
+    arr = tmp;
+
+
+}
+void stack::push(int v){
+    if(sz==cp){duplicate();}
+    arr[sz]=v;
+    sz++;
+}
+void stack::pop(){
+    sz--;
 }
 
-Stack::~Stack()
-{
-    //dtor
+int stack::top(){
+    if(arr[sz-1]==0)
+    return arr[sz-2];
+    else
+        return arr[sz-1];
+}
+
+bool stack::empty(){
+    return sz==0;
+}
+int stack::size(){
+return sz;
+}
+int& stack :: operator[](const int& idx){
+    return arr[idx];
 }
